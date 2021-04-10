c81bc2b3730c0611d2ebe503a502aa948e582a6f
Austin Miller <austin.miller@colorado.edu>
2021-04-10 09:57:03 -0600

    control_flow_intro

---
+++ hello_world/control_flow_intro.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+
+	foo()
+
+	fmt.Println("Back in main")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
